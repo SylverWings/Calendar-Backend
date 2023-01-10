1508cf09477dc91e25f646a5a2a3a29f69f909f9
lionel.garcia <lionel.garcia@t-innova.com>
2023-01-10 14:08:42 +0100

    rename env.MONGO_URL

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const db = () => mongoose.connect(process.env.MONGO_URI)
+const db = () => mongoose.connect(process.env.MONGO_URL)
     .then(()=>{
         console.log("Connection stablish")
     })
